34c9b24b38015425c46e19ac1a1ca75385814eb0
Alessia Smeraglia <alessia.smeraglia@gmail.com>
2026-05-18 18:10:28 +0200

    Aggiunta griglia di card a result section

--- src/components/ResultSection.jsx
+++ src/components/ResultSection.jsx
@@ -1,17 +1,20 @@
 import Card from "./Card";
 
-
 function ResultSection({ title, items, type }) {
         
     return (
-        <section className="mb-4">
-            <h2>{title}</h2>
+        <section className="mb-5">
+            <h2 className="mb-3">{title}</h2>
 
-            <ul className="list-group">
+            <div className="row g-4">
                 {items.map((item) => {
-                    return <Card key={item.id} item={item} type={type} />;
+                    return (
+                        <div key={item.id} className="col-6 col-md-4 col-lg-3 col-xl-2"> 
+                            <Card item={item} type={type} />;
+                        </div>
+                    );
                 })}
-            </ul>
+            </div>
         </section>
     );
 }
