e3ba1e7fc666ef11fc4419ec1a14a0ea9a442fb3
Alessia Smeraglia <alessia.smeraglia@gmail.com>
2026-05-18 18:57:07 +0200

    Migliorato header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,9 +2,9 @@ import SearchBar from "./SearchBar";
 
 function Header() {
     return (
-        <header className="container py-4">
-            <div className="d-flex justify-content-between align-items-center">
-                <h1 className="text-danger">BoolFlix</h1>
+        <header className="bg-black py-3 sticky-top">
+            <div className="container d-flex justify-content-between align-items-center gap-3">
+                <h1 className="text-danger m-0">BoolFlix</h1>
                 <SearchBar />
             </div>
         </header>
