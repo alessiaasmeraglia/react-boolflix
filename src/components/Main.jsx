ea6067cc15c7a444a067f2713b83b89da8a6c363
Alessia Smeraglia <alessia.smeraglia@gmail.com>
2026-05-18 15:12:32 +0200

    Aggiunto componente del main per mostrare la sezionione con i risultati

---
+++ src/components/Main.jsx
@@ -0,0 +1,11 @@
+import ResultSection from "./ResultSection";
+
+function Main() {
+    return (
+        <main className="container py-4">
+            <ResultSection />
+        </main>
+    );
+}
+
+export default Main;
