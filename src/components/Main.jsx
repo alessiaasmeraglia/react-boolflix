58b2549e298d02fad6e694ba40cb0d7a74a80edd
Alessia Smeraglia <alessia.smeraglia@gmail.com>
2026-05-18 16:23:04 +0200

    Aggiornato main che prende dal context sia movies che series

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,9 +1,13 @@
+import { useContext } from "react";
+import GlobalContext from "../contexts/GlobalContext";
 import ResultSection from "./ResultSection";
 
 function Main() {
+    const { movies, series } = useContext(GlobalContext);
     return (
         <main className="container py-4">
-            <ResultSection />
+            <ResultSection title="Film" items={movies} type="movie" />
+            <ResultSection title="Serie TV" items={series} type="series" />
         </main>
     );
 }
