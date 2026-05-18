08bed7183e6040aad607f2b5652a56293218851f
Alessia Smeraglia <alessia.smeraglia@gmail.com>
2026-05-18 17:35:53 +0200

    Aggiornato card con img e voto

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,12 +1,18 @@
 import { getLanguage } from "../utils/flags";
+import { getPosterUrl } from "../utils/images";
+import { getRating } from "../utils/rating";
 
 function Card({ item, type }) {
     const title = type === "movie" ? item.title : item.name;
     const originalTitle = type === "movie" ? item.original_title : item.original_name;
+    
     const language = getLanguage(item.original_language);
+    const posterUrl = getPosterUrl(item.poster_path);
+    const rating = getRating(item.vote_average);
 
     return (
         <li className="list-group-item">
+            <img src={posterUrl} alt={title} className="img-fluid mb-3" />
             <h3>{title}</h3>
 
             <p>
