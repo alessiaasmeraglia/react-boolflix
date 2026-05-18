74318c6ea004b4f43697f826ebbc3965f1bc6aea
Alessia Smeraglia <alessia.smeraglia@gmail.com>
2026-05-18 12:58:11 +0200

    Aggiunto app dentro global provider

--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,20 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
 
 // Bootstrap CSS
 import "bootstrap/dist/css/bootstrap.min.css";
 // Bootstrap JS
 import "bootstrap/dist/js/bootstrap.bundle.min.js";
 
-import './index.css'
-import App from './App.jsx'
+import './index.css';
+
+import GlobalProvider from "./contexts/GlobalProvider.jsx";
+import App from './App.jsx';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <GlobalProvider>
+      <App />
+    </GlobalProvider>
   </StrictMode>,
-)
+);
