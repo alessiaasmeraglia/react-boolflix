848840321e7d5752296472072c59cd2b3d8e7495
Alessia Smeraglia <alessia.smeraglia@gmail.com>
2026-05-19 11:44:08 +0200

    Aggiunta componente per selezionare filtro generi

---
+++ src/components/FilterSelect.jsx
@@ -0,0 +1,33 @@
+import { useContext } from "react";
+import GlobalContext from "../contexts/GlobalContext";
+
+function FilterSelect() {
+    const { movieGenres, seriesGenres, selectedGenre, setSelectedGenre } =
+        useContext(GlobalContext);
+
+    const allGenres = [...movieGenres, ...seriesGenres];
+
+    const uniqueGenres = allGenres.filter((genre, index, array) => {
+        return array.findIndex((item) => item.id === genre.id) === index;
+    });
+
+    return (
+        <select
+            className="form-select"
+            value={selectedGenre}
+            onChange={(event) => setSelectedGenre(event.target.value)}
+        >
+            <option value="">Tutti i generi</option>
+
+            {uniqueGenres.map((genre) => {
+                return (
+                    <option key={genre.id} value={genre.id}>
+                        {genre.name}
+                    </option>
+                );
+            })}
+        </select>
+    );
+}
+
+export default FilterSelect;
