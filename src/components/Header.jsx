88b4c77f83fd749506cc20c0a29ced61bab4edec
Alessia Smeraglia <alessia.smeraglia@gmail.com>
2026-05-19 11:46:42 +0200

    Aggiunto filtro nell'header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,17 @@
 import SearchBar from "./SearchBar";
+import FilterSelect from "./FilterSelect";
 
 function Header() {
     return (
         <header className="bg-black py-3 sticky-top">
             <div className="container d-flex justify-content-between align-items-center gap-3">
                 <h1 className="text-danger m-0">BoolFlix</h1>
-                <SearchBar />
+
+                <div  className="d-flex gap-2">
+                    <FilterSelect />
+                    <SearchBar />
+                </div>
+                
             </div>
         </header>
     );
