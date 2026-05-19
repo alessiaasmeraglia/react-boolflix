42d749e403c2f2cb3c328946670fc29923fe89c5
Alessia Smeraglia <alessia.smeraglia@gmail.com>
2026-05-19 12:34:04 +0200

    Aggiunto main al layout e tolto dai singoli componenti

---
+++ src/layouts/MainLayout.jsx
@@ -0,0 +1,16 @@
+import { Outlet } from "react-router-dom";
+import Header from "../components/Header";
+
+function MainLayout() {
+    return (
+        <>
+            <Header />
+
+            <main className="container py-4">
+                <Outlet />
+            </main>
+        </>
+    );
+}
+
+export default MainLayout;
