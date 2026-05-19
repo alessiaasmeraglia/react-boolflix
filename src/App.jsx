14aa5b31a08bc8d6401dec0a852ffd27978b5d19
Alessia Smeraglia <alessia.smeraglia@gmail.com>
2026-05-19 12:37:29 +0200

    Aggiunte rotte gestite da App

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,21 @@
-import { useState } from 'react';
-import Header from "./components/Header";
-import Main from './components/Main';
+import { Routes, Route } from "react-router-dom";
+import MainLayout from "./layouts/MainLayout";
+import Home from "./pages/Home";
+import MoviesPage from "./pages/MoviesPage";
+import SeriesPage from "./pages/SeriesPage";
+import NotFound from "./pages/NotFound";
 
 function App() {
     return (
-        <>
-            <Header />
-            <Main />
-        </>
+        <Routes>
+            <Route element={<MainLayout />}>
+                <Route index element={<Home />} />
+                <Route path="movies" element={<MoviesPage />} />
+                <Route path="series" element={<SeriesPage />} />
+                <Route path="*" element={<NotFound />} />
+            </Route>
+        </Routes>
     );
-
 }
 
 export default App;
